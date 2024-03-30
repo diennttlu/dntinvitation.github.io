8ef89db37940d2333d9544ad63a986e7dce846ad
diennt <diennttlu@gmail.com>
2024-03-31 03:13:07 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+var photoGalleries = [];
+
+photoGalleries.push({
+    src: "./img/anh le (19).jpg",
+    thumb: "./img/anh le (19).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (18).jpg",
+    thumb: "./img/anh le (18).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (17).jpg",
+    thumb: "./img/anh le (17).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (16).jpg",
+    thumb: "./img/anh le (16).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (15).jpg",
+    thumb: "./img/anh le (15).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (26).jpg",
+    thumb: "./img/anh le (26).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (27).jpg",
+    thumb: "./img/anh le (27).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (13).jpg",
+    thumb: "./img/anh le (13).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (12).jpg",
+    thumb: "./img/anh le (12).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (10).jpg",
+    thumb: "./img/anh le (10).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+photoGalleries.push({
+    src: "./img/anh le (4).jpg",
+    thumb: "./img/anh le (4).jpg",
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`
+});
+                    
